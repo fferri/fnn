35c22154851faa3e1ab01d6c9a75a0b31d11812b
federico.ferri.it@gmail.com <federico.ferri.it@gmail.com@38864ba2-56e3-6e65-57c3-6a700ae405ef>
2012-11-19 13:14:19 +0000

    UNIX-style character filter

--- filter.cpp
+++ filter.cpp
@@ -4,41 +4,17 @@
 
 using namespace std;
 
-typedef istreambuf_iterator<char> buf_iter;
-
-int map[256] = {0};
-int numChars;
-
-void initTable() {
-    numChars = 0;
-    for(unsigned char c = 0; c <= 127; c++) {
-        if(isalnum(c) || ispunct(c) || c == ' ')
-            map[c] = ++numChars;
-    }
-}
-
-void makeVector(int i, ofstream& out) {
-    for(int j = 1; j <= numChars; j++) {
-        if(j > 1) out << " ";
-        out << ((j == i) ? "1" : "0");
-    }
-    out << endl;
-}
+#define iswhitespace(x) ((x) == '\n' || (x) == '\t' || (x) == ' ')
 
 int main(int argc, char **argv) {
-    initTable();
 	char c;
     int space = 0;
-    int first = 1;
     while(cin.good()) {
         cin.get(c);
-        if(isalnum(c) || ispunct(c) || c == ' ' || c == '\n') {
-            if(c == '\n') c = ' ';
-            if(c != ' ' || !space) {
-                cout << c;
-            }
-            if(c == ' ') space = 1;
-            else space = 0;
+        if(isalnum(c) || ispunct(c) || iswhitespace(c)) {
+            if(iswhitespace(c)) c = ' ';
+            if(c != ' ' || !space) cout << c;
+            space = (c == ' ');
         }
 	}
     return 0;
